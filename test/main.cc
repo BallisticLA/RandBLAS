c89016052efc0c7bcc57f667f07e83a138806d31
rileyjmurray <rjmurray@berkeley.edu>
2022-02-22 10:12:52 -0800

    can compile and run an empty test suite

--- test/main.cc
+++ test/main.cc
@@ -1,14 +1,18 @@
 #include <sparseops.hh>
 #include <blas.hh>
+#include "gtest/gtest.h"
 
 
-int main(int argc, const char *argv[]) {
-    int64_t n = 20;
-    int64_t m = 2*n;
-    run_pcgls_ex(n, m);
+int main(int argc, char *argv[]) {
+    //int64_t n = 20;
+    //int64_t m = 2*n;
+    //run_pcgls_ex(n, m);
 
     double a[25];
     int64_t k = 5;
 	genmat(k, k, a, 0); 
-    return 0;
+     
+    ::testing::InitGoogleTest(&argc, argv);
+    int res = RUN_ALL_TESTS();
+    return res;
 }
