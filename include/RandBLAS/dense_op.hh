8aa5f542e712621b1e6f670e1f3310cc032cf141
Riley Murray <rjmurray@berkeley.edu>
2022-11-30 11:43:12 -0800

    reformat files

--- include/RandBLAS/dense_op.hh
+++ include/RandBLAS/dense_op.hh
@@ -17,21 +17,35 @@
 
 namespace RandBLAS::dense_op {
 
+enum class DenseDist : char {Gaussian = 'G', Normal = 'G', Uniform = 'U', Rademacher = 'R', Haar = 'H'};
+
+template <typename T>
+struct SketchingBuffer {
+    DenseDist dist = DenseDist::Gaussian;
+    int64_t ctr_offset = 0;
+    int64_t key = 0;
+    int64_t n_rows;
+    int64_t n_cols;
+    T *op_data = NULL;
+    bool populated = false;
+    bool persistent = false;
+}
+
 template <typename T>
 void gen_rmat_unif(
-        int64_t n_rows,
-        int64_t n_cols,
-        T* mat,
-        uint32_t seed
-        );
+    int64_t n_rows,
+    int64_t n_cols,
+    T* mat,
+    uint32_t seed
+);
 
 template <typename T>
 void gen_rmat_norm(
-        int64_t n_rows,
-        int64_t n_cols,
-        T* mat,
-        uint32_t seed
-        );
+    int64_t n_rows,
+    int64_t n_cols,
+    T* mat,
+    uint32_t seed
+);
 
 } // end namespace RandBLAS::dense_op
 
