1106843db002836d1ffcdf3d2e3d2f2536062161
Burlen Loring <bloring@lbl.gov>
2022-12-07 10:23:39 -0800

    build support use as a git submodule

--- CMake/rb_config.cmake
+++ CMake/rb_config.cmake
@@ -1,5 +1,6 @@
 configure_file(RandBLAS_config.h.in RandBLAS_config.h)
-install(FILES ${CMAKE_BINARY_DIR}/RandBLAS_config.h DESTINATION include/RandBLAS)
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/RandBLAS_config.h
+    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/RandBLAS)
 
 configure_file(CMake/RandBLASConfig.cmake.in
     ${CMAKE_INSTALL_LIBDIR}/cmake/RandBLASConfig.cmake @ONLY)
@@ -8,6 +9,6 @@ configure_file(CMake/RandBLASConfigVersion.cmake.in
     ${CMAKE_INSTALL_LIBDIR}/cmake/RandBLASConfigVersion.cmake @ONLY)
 
 install(FILES
-    ${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake/RandBLASConfig.cmake
-    ${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake/RandBLASConfigVersion.cmake
+    ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake/RandBLASConfig.cmake
+    ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake/RandBLASConfigVersion.cmake
     DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake)
