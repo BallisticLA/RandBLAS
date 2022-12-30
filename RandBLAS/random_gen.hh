b5bdf79a5c311a0dcadf8e41eda64b309940f345
Burlen Loring <bloring@lbl.gov>
2022-12-30 11:18:46 -0800

    remove uneccessary structured binding code for r123::float2

--- RandBLAS/random_gen.hh
+++ RandBLAS/random_gen.hh
@@ -15,22 +15,4 @@
 #include <Random123/boxmuller.hpp>
 #include <Random123/uniform.hpp>
 
-/// @cond
-/// extend r123::float2 to work with structured bindings
-namespace std {
-  template<> struct tuple_size<r123::float2> { static constexpr size_t value = 2; };
-  template<> struct tuple_element<0, r123::float2> { using type = float; };
-  template<> struct tuple_element<1, r123::float2> { using type = float; };
-}
-
-namespace r123 {
-template<std::size_t I>
-std::tuple_element_t<I, r123::float2> get(r123::float2 const& f2)
-{
-  if constexpr (I == 0) return f2.x;
-  if constexpr (I == 1) return f2.y;
-}
-}
-/// @endcond
-
 #endif
