b0ddaaad6d8197775a4a265bccbe896d59cde3a2
KaiwenHe <kaiwen.he7@gmail.com>
2023-06-13 12:29:53 -0700

    Fixed counter bug for fill_dense_submat_impl

--- RandBLAS/dense.hh
+++ RandBLAS/dense.hh
@@ -266,6 +266,7 @@ static auto fill_dense_submat_impl(
     }
 
     }
+    c.incr( (int64_t) (ptr + n_scols-1 + (n_srows-1) * n_cols) / RNG::ctr_type::static_size);
     return RNGState<RNG> {c, k};
 } 
 
