649ad445383be408f9635e386e2c6a2e54061007
KaiwenHe <kaiwen.he7@gmail.com>
2023-07-06 16:32:01 -0700

    Added comments and cleaned up depreciated code

--- RandBLAS/dense.hh
+++ RandBLAS/dense.hh
@@ -308,7 +308,9 @@ static RandBLAS::RNGState<RNG> fill_dense_submat_impl(
     }
 
     }
-
+    
+    //max_thrd is represents the number of threads that were used in the parallel for loop.
+    //The number of threads used in the parallel for loop is not necesarrily equal to num_threads, especially if the matrix is small.
     int max_thrd = thrd_arr[0];
     for (int i = 1; i < num_thrds; i++) {
         if (thrd_arr[i] > max_thrd) {
@@ -316,16 +318,13 @@ static RandBLAS::RNGState<RNG> fill_dense_submat_impl(
         }
     }
 
+    //finds the largest counter in the counter array
     typename RNG::ctr_type max_c = ctr_arr[0];
     for (int i = 1; i < max_thrd+1; i++) {  
         if (compare_ctr<RNG>(ctr_arr[i], max_c) == true) {
             max_c = ctr_arr[i];
         }
     }
-
-    /*int64_t last_ptr = ptr + n_scols-1 + (n_srows-1)*n_cols;
-    int64_t last_ptr_padded = last_ptr + last_ptr/n_cols * pad;
-    c.incr(last_ptr_padded / RNG::ctr_type::static_size + 1);*/
 
     max_c.incr();
     return RNGState<RNG> {max_c, k};
