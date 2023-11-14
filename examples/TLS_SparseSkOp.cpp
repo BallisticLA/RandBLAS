dc72edcab40cc8cf3953eaea0dcb3925f9ed3c53
KaiwenHe <kaiwen.he7@gmail.com>
2023-11-14 15:29:06 -0500

    Made requested modifications.

--- examples/TLS_SparseSkOp.cpp
+++ examples/TLS_SparseSkOp.cpp
@@ -42,42 +42,52 @@ void init_noisy_data(int64_t m, int64_t n, int64_t d, double* AB){
     }
 }
 
+/* Let A be a tall data matrix of dimensions m by n where m > n and b be a vector of dimension m.
+ * In ordinary least squares it assumes that the error lies only in the right hand side vector b,
+ * and it aims to find a vector x that minimizes ||A*x - b||_2.
+ * On the other hand, total least squares assumes that the input data matrix A could also incur errors.
+ * Total least squares aims to find a solution where the error is orthogonal to the regression model.
+ */
+
+// To call the executable run ./TLS_DenseSkOp <m> <n> where <m> <n> are the number of rows and columns
+// of A respectively. We expect m > 2*n.
 int main(int argc, char* argv[]){
-    // Goal: Solve total least squares problem ||AX - B|| 
 
     // Initialize dimensions
     int64_t m;           // Number of rows of A, B
     int64_t n;           // Number of columns of A
-    int64_t d = 1;       // Number of columns of B
                          
-    if (argc == 3) {
+    if (argc == 1) {
+        m = 10000;
+        n = 500;
+    } else if (argc == 3) {
         m = atoi(argv[1]);
         n = atoi(argv[2]);
-        if (2*(n+d) > m) {
-            std::cout << "Make sure number of rows are double the number of cols" << '\n';
+        if (n > m) {
+            std::cout << "Make sure number of rows are greater than number of cols" << '\n';
             exit(0);
         }
     } else {
-        m = 10000;
-        n = 500;
-    }   
-    
+        std::cout << "Invalid arguments" << '\n';
+        exit(1);
+    }
+
     // Define number or rows of the sketching operator
-    int64_t sk_dim = 2*(n+d);
+    int64_t sk_dim = 2*(n+1);
 
     // Initialize workspace
-    double *AB = new double[m*(n + d)]; // Store [A B] in column major format
-    double *SAB = new double[sk_dim*(n+d)];
+    double *AB = new double[m*(n+1)]; // Store [A B] in column major format
+    double *SAB = new double[sk_dim*(n+1)];
     double *X = new double[n];
     double *res = new double[n];
 
     // Initialize workspace for the sketched svd 
     double *U = new double[sk_dim*sk_dim];
-    double *svals = new double[n+d];
-    double *VT = new double[(n+d)*(n+d)];
+    double *svals = new double[n+1];
+    double *VT = new double[(n+1)*(n+1)];
 
     // Initialize noisy gaussian data
-    init_noisy_data(m, n, d, AB);
+    init_noisy_data(m, n, 1, AB);
 
 
     // Define the parameters of the sparse distribution 
@@ -104,12 +114,10 @@ int main(int argc, char* argv[]){
             blas::Op::NoTrans,         // NoTrans => \op(S) = S, Trans => \op(S) = S^T
             blas::Op::NoTrans,         // NoTrans => \op(AB) = AB, Trans => \op(AB) = AB^T
             sk_dim,                    // Number of rows of S and SAB
-            n+d,                       // Number of columns of AB and SAB
+            n+1,                       // Number of columns of AB and SAB
             m,                         // Number of rows of AB and columns of S
             1,                         // Scalar alpha - if alpha is zero AB is not accessed
             S,                         // A DenseSkOp or SparseSkOp sketching operator
-            0,                         // Row offset of S
-            0,                         // Column offset of S
             AB,                        // Matrix to be sketched
             m,                         // Leading dimension of AB
             0,                         // Scalar beta - if beta is zero SAB is not accessed
@@ -120,14 +128,14 @@ int main(int argc, char* argv[]){
 
     // Perform SVD operation on SAB
     auto time_TLS1 = high_resolution_clock::now();
-    lapack::gesdd(lapack::Job::AllVec, sk_dim, (n+d), SAB, sk_dim, svals, U, sk_dim, VT, n+d);
+    lapack::gesdd(lapack::Job::AllVec, sk_dim, (n+1), SAB, sk_dim, svals, U, sk_dim, VT, n+1);
             
     for (int i = 0; i < n; i++) {
-        X[i] = VT[(n+d-1) + i*(n+d)]; // Take the right n by 1 block of V
+        X[i] = VT[n + i*(n+1)]; // Take the right n by 1 block of V
     }
 
     // Scale X by the inverse of the 1 by 1 bottom right block of V
-    blas::scal(n, -1/VT[(n+d)*(n+d)-1], X, 1); 
+    blas::scal(n, -1/VT[(n+1)*(n+1)-1], X, 1); 
     auto time_TLS2 = high_resolution_clock::now();
 
     //Check TLS solution. Expected to be a vector of 1's
@@ -142,13 +150,13 @@ int main(int argc, char* argv[]){
         }
     }
 
-    std::cout << "Matrix dimensions:                              " << m << " by " << n+d << '\n'; 
-    std::cout << "Sketch dimension:                               " << sk_dim << '\n'; 
-    std::cout << "Time to create sparse sketch:                   " << (double) duration_cast<milliseconds>(time_constructsketch2 - time_constructsketch1).count()/1000 << " seconds" << '\n';
-    std::cout << "Time to sketch AB:                              " << (double) duration_cast<milliseconds>(time_sketch2 - time_sketch1).count()/1000 << " seconds" <<'\n';
-    std::cout << "Time to perform TLS on sketched matrix:         " << (double) duration_cast<milliseconds>(time_TLS2 - time_TLS1).count()/1000 << " seconds" << '\n';
-    std::cout << "Inf-norm of TLS residal vector:                 " << res_infnorm << '\n';
-    std::cout << "Two-norm of TLS residual vector:                " << sqrt(res_twonorm) << '\n';
+    std::cout << "Matrix dimensions:                                                " << m << " by " << n+1 << '\n'; 
+    std::cout << "Sketch dimension:                                                 " << sk_dim << '\n'; 
+    std::cout << "Time to create dense sketch:                                      " << (double) duration_cast<milliseconds>(time_constructsketch2 - time_constructsketch1).count()/1000 << " seconds" << '\n';
+    std::cout << "Time to sketch AB:                                                " << (double) duration_cast<milliseconds>(time_sketch2 - time_sketch1).count()/1000 << " seconds" <<'\n';
+    std::cout << "Time to perform TLS on sketched matrix:                           " << (double) duration_cast<milliseconds>(time_TLS2 - time_TLS1).count()/1000 << " seconds" << '\n';
+    std::cout << "Inf-norm distance from TLS solution to vector of all ones:        " << res_infnorm << '\n';
+    std::cout << "Two-norm distance from TLS solution to vector of all ones:        " << sqrt(res_twonorm) << '\n';
 
     delete[] AB;
     delete[] SAB;
