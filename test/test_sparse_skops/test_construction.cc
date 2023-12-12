3485d6297f6e7b30b9ef9e20d39776c6d87784a7
Riley Murray <rjmurr@sandia.gov>
2023-12-12 08:31:07 -0500

    clean up tests

--- test/test_sparse_skops/test_construction.cc
+++ test/test_sparse_skops/test_construction.cc
@@ -16,8 +16,9 @@ class TestSparseSkOpConstruction : public ::testing::Test
 
     virtual void TearDown() {};
 
-    template <typename T = double, typename RNG = RandBLAS::SparseSkOp<double>::RNG_t, RandBLAS::SignedInteger sint_t>
-    void check_fixed_nnz_per_col(RandBLAS::SparseSkOp<T,RNG,sint_t> &S0) {
+    template <typename SKOP>
+    void check_fixed_nnz_per_col(SKOP &S0) {
+        using sint_t = typename SKOP::index_t;
         std::set<sint_t> s;
         for (int64_t i = 0; i < S0.dist.n_cols; ++i) {
             int64_t offset = S0.dist.vec_nnz * i;
@@ -30,8 +31,9 @@ class TestSparseSkOpConstruction : public ::testing::Test
         }
     }
 
-    template <typename T = double, typename RNG = RandBLAS::SparseSkOp<double>::RNG_t, RandBLAS::SignedInteger sint_t>
-    void check_fixed_nnz_per_row(RandBLAS::SparseSkOp<T,RNG,sint_t> &S0) {
+    template <typename SKOP>
+    void check_fixed_nnz_per_row(SKOP &S0) {
+        using sint_t = typename SKOP::index_t;
         std::set<sint_t> s;
         for (int64_t i = 0; i < S0.dist.n_rows; ++i) {
             int64_t offset = S0.dist.vec_nnz * i;
@@ -44,53 +46,32 @@ class TestSparseSkOpConstruction : public ::testing::Test
         }
     }
 
-    virtual void proper_saso_construction(int64_t d, int64_t m, int64_t key_index, int64_t nnz_index, bool use_int=false) {
-        if (!use_int) {
-            RandBLAS::SparseSkOp<double> S0(
-                {d, m, vec_nnzs[nnz_index], RandBLAS::MajorAxis::Short}, keys[key_index]
-            );
-            RandBLAS::fill_sparse(S0);
-            if (d < m) {
-                    check_fixed_nnz_per_col(S0);
-            } else {
-                    check_fixed_nnz_per_row(S0);
-            }
+    template <RandBLAS::SignedInteger sint_t>
+    void proper_saso_construction(int64_t d, int64_t m, int64_t key_index, int64_t nnz_index) {
+        using RNG = RandBLAS::SparseSkOp<float>::RNG_t;
+        RandBLAS::SparseSkOp<float, RNG, sint_t> S0(
+            {d, m, vec_nnzs[nnz_index], RandBLAS::MajorAxis::Short}, keys[key_index]
+        );
+        RandBLAS::fill_sparse(S0);
+        if (d < m) {
+                check_fixed_nnz_per_col(S0);
         } else {
-            using RNG = RandBLAS::SparseSkOp<double>::RNG_t;
-            RandBLAS::SparseSkOp<double, RNG, int> S0(
-                {d, m, vec_nnzs[nnz_index], RandBLAS::MajorAxis::Short}, keys[key_index]
-            );
-            RandBLAS::fill_sparse(S0);
-            if (d < m) {
-                    check_fixed_nnz_per_col(S0);
-            } else {
-                    check_fixed_nnz_per_row(S0);
-            }
+                check_fixed_nnz_per_row(S0);
         }
-    } 
+    }
 
-    virtual void proper_laso_construction(int64_t d, int64_t m, int64_t key_index, int64_t nnz_index, bool use_int=false) {
-        if (!use_int) {
-            RandBLAS::SparseSkOp<double> S0(
-                {d, m, vec_nnzs[nnz_index], RandBLAS::MajorAxis::Long}, keys[key_index]
-            );
-            RandBLAS::fill_sparse(S0);
-            if (d < m) {
-                    check_fixed_nnz_per_row(S0);
-            } else {
-                    check_fixed_nnz_per_col(S0);
-            } 
+    template <RandBLAS::SignedInteger sint_t>
+    void proper_laso_construction(int64_t d, int64_t m, int64_t key_index, int64_t nnz_index) {
+        using RNG = RandBLAS::SparseSkOp<float>::RNG_t;
+        RandBLAS::SparseSkOp<float, RNG, sint_t> S0(
+            {d, m, vec_nnzs[nnz_index], RandBLAS::MajorAxis::Long}, keys[key_index]
+        );
+        RandBLAS::fill_sparse(S0);
+        if (d < m) {
+                check_fixed_nnz_per_row(S0);
         } else {
-            RandBLAS::SparseSkOp<double,r123::Philox4x32,int> S0(
-                {d, m, vec_nnzs[nnz_index], RandBLAS::MajorAxis::Long}, keys[key_index]
-            );
-            RandBLAS::fill_sparse(S0);
-            if (d < m) {
-                    check_fixed_nnz_per_row(S0);
-            } else {
-                    check_fixed_nnz_per_col(S0);
-            } 
-        }
+                check_fixed_nnz_per_col(S0);
+        } 
     }
 };
 
@@ -105,53 +86,55 @@ class TestSparseSkOpConstruction : public ::testing::Test
 
 TEST_F(TestSparseSkOpConstruction, SASO_Dim_7by20) {
     // vec_nnz=1
-    proper_saso_construction(7, 20, 0, 0);
-    proper_saso_construction(7, 20, 1, 0);
-    proper_saso_construction(7, 20, 2, 0);
+    proper_saso_construction<int64_t>(7, 20, 0, 0);
+    proper_saso_construction<int64_t>(7, 20, 1, 0);
+    proper_saso_construction<int64_t>(7, 20, 2, 0);
     // vec_nnz=2
-    proper_saso_construction(7, 20, 0, 1);
-    proper_saso_construction(7, 20, 1, 1);
-    proper_saso_construction(7, 20, 2, 1);
+    proper_saso_construction<int64_t>(7, 20, 0, 1);
+    proper_saso_construction<int64_t>(7, 20, 1, 1);
+    proper_saso_construction<int64_t>(7, 20, 2, 1);
     // vec_nnz=3
-    proper_saso_construction(7, 20, 0, 2);
-    proper_saso_construction(7, 20, 1, 2);
-    proper_saso_construction(7, 20, 2, 2);
+    proper_saso_construction<int64_t>(7, 20, 0, 2);
+    proper_saso_construction<int64_t>(7, 20, 1, 2);
+    proper_saso_construction<int64_t>(7, 20, 2, 2);
     // vec_nnz=7
-    proper_saso_construction(7, 20, 0, 3);
-    proper_saso_construction(7, 20, 1, 3);
-    proper_saso_construction(7, 20, 2, 3);
+    proper_saso_construction<int64_t>(7, 20, 0, 3);
+    proper_saso_construction<int64_t>(7, 20, 1, 3);
+    proper_saso_construction<int64_t>(7, 20, 2, 3);
 }
 
 
 TEST_F(TestSparseSkOpConstruction, SASO_Dim_15by7) {
     // vec_nnz=1
-    proper_saso_construction(15, 7, 0, 0);
-    proper_saso_construction(15, 7, 1, 0);
+    proper_saso_construction<int64_t>(15, 7, 0, 0);
+    proper_saso_construction<int64_t>(15, 7, 1, 0);
     // vec_nnz=1
-    proper_saso_construction(15, 7, 0, 1);
-    proper_saso_construction(15, 7, 1, 1);
+    proper_saso_construction<int64_t>(15, 7, 0, 1);
+    proper_saso_construction<int64_t>(15, 7, 1, 1);
     // vec_nnz=3
-    proper_saso_construction(15, 7, 0, 2);
-    proper_saso_construction(15, 7, 1, 2);
+    proper_saso_construction<int64_t>(15, 7, 0, 2);
+    proper_saso_construction<int64_t>(15, 7, 1, 2);
     // vec_nnz=7
-    proper_saso_construction(15, 7, 0, 3);
-    proper_saso_construction(15, 7, 1, 3);
+    proper_saso_construction<int64_t>(15, 7, 0, 3);
+    proper_saso_construction<int64_t>(15, 7, 1, 3);
 }
 
 
 TEST_F(TestSparseSkOpConstruction, SASO_Dim_7by20_int32) {
-    proper_saso_construction(7, 20, 0, 0, true);
-    proper_saso_construction(7, 20, 0, 1, true);
-    proper_saso_construction(7, 20, 0, 2, true);
-    proper_saso_construction(7, 20, 0, 3, true);
+    // test vec_nnz = 1, 2, 3, 7
+    proper_saso_construction<int>(7, 20, 0, 0);
+    proper_saso_construction<int>(7, 20, 0, 1);
+    proper_saso_construction<int>(7, 20, 0, 2);
+    proper_saso_construction<int>(7, 20, 0, 3);
 }
 
 
 TEST_F(TestSparseSkOpConstruction, SASO_Dim_15by7_int32) {
-    proper_saso_construction(15, 7, 0, 0, true);
-    proper_saso_construction(15, 7, 0, 1, true);
-    proper_saso_construction(15, 7, 0, 2, true);
-    proper_saso_construction(15, 7, 0, 3, true);
+    // test vec_nnz = 1, 2, 3, 7
+    proper_saso_construction<int>(15, 7, 0, 0);
+    proper_saso_construction<int>(15, 7, 0, 1);
+    proper_saso_construction<int>(15, 7, 0, 2);
+    proper_saso_construction<int>(15, 7, 0, 3);
 }
 
 
@@ -165,71 +148,71 @@ TEST_F(TestSparseSkOpConstruction, SASO_Dim_15by7_int32) {
 
 TEST_F(TestSparseSkOpConstruction, LASO_Dim_7by20) {
     // vec_nnz=1
-    proper_laso_construction(7, 20, 0, 0);
-    proper_laso_construction(7, 20, 1, 0);
-    proper_laso_construction(7, 20, 2, 0);
+    proper_laso_construction<int64_t>(7, 20, 0, 0);
+    proper_laso_construction<int64_t>(7, 20, 1, 0);
+    proper_laso_construction<int64_t>(7, 20, 2, 0);
     // vec_nnz=2
-    proper_laso_construction(7, 20, 0, 1);
-    proper_laso_construction(7, 20, 1, 1);
-    proper_laso_construction(7, 20, 2, 1);
+    proper_laso_construction<int64_t>(7, 20, 0, 1);
+    proper_laso_construction<int64_t>(7, 20, 1, 1);
+    proper_laso_construction<int64_t>(7, 20, 2, 1);
     // vec_nnz=3
-    proper_laso_construction(7, 20, 0, 2);
-    proper_laso_construction(7, 20, 1, 2);
-    proper_laso_construction(7, 20, 2, 2);
+    proper_laso_construction<int64_t>(7, 20, 0, 2);
+    proper_laso_construction<int64_t>(7, 20, 1, 2);
+    proper_laso_construction<int64_t>(7, 20, 2, 2);
     // vec_nnz=7
-    proper_laso_construction(7, 20, 0, 3);
-    proper_laso_construction(7, 20, 1, 3);
-    proper_laso_construction(7, 20, 2, 3);
+    proper_laso_construction<int64_t>(7, 20, 0, 3);
+    proper_laso_construction<int64_t>(7, 20, 1, 3);
+    proper_laso_construction<int64_t>(7, 20, 2, 3);
 }
 
 
 TEST_F(TestSparseSkOpConstruction, LASO_Dim_15by7) {
     // vec_nnz=1
-    proper_laso_construction(15, 7, 0, 0);
-    proper_laso_construction(15, 7, 1, 0);
+    proper_laso_construction<int64_t>(15, 7, 0, 0);
+    proper_laso_construction<int64_t>(15, 7, 1, 0);
     // vec_nnz=2
-    proper_laso_construction(15, 7, 0, 1);
-    proper_laso_construction(15, 7, 1, 1);
+    proper_laso_construction<int64_t>(15, 7, 0, 1);
+    proper_laso_construction<int64_t>(15, 7, 1, 1);
     // vec_nnz=3
-    proper_laso_construction(15, 7, 0, 2);
-    proper_laso_construction(15, 7, 1, 2);
+    proper_laso_construction<int64_t>(15, 7, 0, 2);
+    proper_laso_construction<int64_t>(15, 7, 1, 2);
     // vec_nnz=7
-    proper_laso_construction(15, 7, 0, 3);
-    proper_laso_construction(15, 7, 1, 3);
+    proper_laso_construction<int64_t>(15, 7, 0, 3);
+    proper_laso_construction<int64_t>(15, 7, 1, 3);
 }
 
 
 TEST_F(TestSparseSkOpConstruction, LASO_Dim_7by20_int32) {
     // vec_nnz=1
-    proper_laso_construction(7, 20, 0, 0, true);
-    proper_laso_construction(7, 20, 1, 0, true);
-    proper_laso_construction(7, 20, 2, 0, true);
+    proper_laso_construction<int>(7, 20, 0, 0);
+    proper_laso_construction<int>(7, 20, 1, 0);
+    proper_laso_construction<int>(7, 20, 2, 0);
     // vec_nnz=2
-    proper_laso_construction(7, 20, 0, 1, true);
-    proper_laso_construction(7, 20, 1, 1, true);
-    proper_laso_construction(7, 20, 2, 1, true);
+    proper_laso_construction<int>(7, 20, 0, 1);
+    proper_laso_construction<int>(7, 20, 1, 1);
+    proper_laso_construction<int>(7, 20, 2, 1);
     // vec_nnz=3
-    proper_laso_construction(7, 20, 0, 2, true);
-    proper_laso_construction(7, 20, 1, 2, true);
-    proper_laso_construction(7, 20, 2, 2, true);
+    proper_laso_construction<int>(7, 20, 0, 2);
+    proper_laso_construction<int>(7, 20, 1, 2);
+    proper_laso_construction<int>(7, 20, 2, 2);
     // vec_nnz=7
-    proper_laso_construction(7, 20, 0, 3, true);
-    proper_laso_construction(7, 20, 1, 3, true);
-    proper_laso_construction(7, 20, 2, 3, true);
+    proper_laso_construction<int>(7, 20, 0, 3);
+    proper_laso_construction<int>(7, 20, 1, 3);
+    proper_laso_construction<int>(7, 20, 2, 3);
 }
 
 
 TEST_F(TestSparseSkOpConstruction, LASO_Dim_15by7_int32) {
     // vec_nnz=1
-    proper_laso_construction(15, 7, 0, 0, true);
-    proper_laso_construction(15, 7, 1, 0, true);
+    proper_laso_construction<int>(15, 7, 0, 0);
+    proper_laso_construction<int>(15, 7, 1, 0);
     // vec_nnz=2
-    proper_laso_construction(15, 7, 0, 1, true);
-    proper_laso_construction(15, 7, 1, 1, true);
+    proper_laso_construction<int>(15, 7, 0, 1);
+    proper_laso_construction<int>(15, 7, 1, 1);
     // vec_nnz=3
-    proper_laso_construction(15, 7, 0, 2, true);
-    proper_laso_construction(15, 7, 1, 2, true);
+    proper_laso_construction<int>(15, 7, 0, 2);
+    proper_laso_construction<int>(15, 7, 1, 2);
     // vec_nnz=7
-    proper_laso_construction(15, 7, 0, 3, true);
-    proper_laso_construction(15, 7, 1, 3, true);
+    proper_laso_construction<int>(15, 7, 0, 3);
+    proper_laso_construction<int>(15, 7, 1, 3);
 }
