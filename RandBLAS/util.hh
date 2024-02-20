33e4d1937c82e9658faed6e0a9b2a53ef24c209a
rileyjmurray <rjmurray@berkeley.edu>
2024-02-20 11:04:12 -0800

    add utility function (not strictly needed, for the current branch, but I might as well check it in)

--- RandBLAS/util.hh
+++ RandBLAS/util.hh
@@ -10,6 +10,38 @@
 namespace RandBLAS::util {
 
 template <typename T>
+void increase_stride(T* buff, int64_t cur_stride, int64_t new_stride, int64_t num_vecs) {
+    // The first (num_vecs * cur_stride) entries of buff define "num_vecs" vectors of
+    // length "cur_stride".
+    //
+    // buff has at least (num_vecs * new_stride) space.
+    //
+    // We'll move data farther down buff so each of the num_vecs vectors has
+    //      shift = new_stride - cur_stride
+    // zeros after it (shift >= 0).
+    //
+    int64_t incr = new_stride - cur_stride;
+    randblas_require(incr >= 0);
+    T* work = new T[cur_stride];
+    for (int64_t i = num_vecs - 1; i >= 1; --i) {
+        // copy the current vector into temporary workspace
+        T* vec_tip_before = &buff[i*cur_stride];
+        blas::copy(cur_stride, vec_tip_before, 1, work, 1);
+        // copy the workspace into the new position of the vector
+        T* vec_tip_after = vec_tip_before + i*incr;
+        blas::copy(cur_stride, work, 1, vec_tip_after, 1);
+    }
+    delete [] work;
+    // zero out "incr" entries after each vector.
+    T* vec_tail = &buff[cur_stride];
+    for (int64_t i = 0; i < num_vecs; ++i) {
+        for (int64_t j = 0; j < incr; ++j)
+            vec_tail[j] = 0.0;
+        vec_tail = vec_tail + new_stride;
+    }
+}
+
+template <typename T>
 void safe_scal(int64_t n, T a, T* x, int64_t inc_x) {
     if (a == 0.0) {
         for (int64_t i = 0; i < n; ++i)
