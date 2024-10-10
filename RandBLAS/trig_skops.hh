10470bb8f88d400f70f6db7bb431c6ffcd11be4a
Aryaman Jeendgar <jeendgararyaman@gmail.com>
2024-10-10 15:23:04 -0700

    Removed old `TrigSkOp` code

--- RandBLAS/trig_skops.hh
+++ RandBLAS/trig_skops.hh
@@ -28,10 +28,8 @@ namespace RandBLAS {
     /// public API
     ///
     template<SignedInteger sint_t>
-    // void generateRademacherVector_r123(std::vector<int64_t> &buff, uint32_t key_seed, uint32_t ctr_seed, int64_t n) {
     void generateRademacherVector_r123(sint_t* buff, uint32_t key_seed, uint32_t ctr_seed, int64_t n) {
         typedef r123::Philox2x32 RNG;
-        // std::vector<int64_t> rademacherVector(n);
 
         // Use OpenMP to parallelize the Rademacher vector generation
         #pragma omp parallel
@@ -387,176 +385,10 @@ namespace RandBLAS {
         else
             fht_right_row_major(buff, log_n, num_rows, num_cols);
     }
-
-    enum class TrigDistName: char {
-        Fourier = 'F',
-
-        // ---------------------------------------------------------------------------
-
-        Hadamard = 'H'
-    };
-
-    struct TrigDist {
-        const int64_t n_rows;
-        const int64_t n_cols;
-
-        int64_t dim_short;
-        int64_t dim_long;
-
-        const TrigDistName family;
-
-        TrigDist(
-            int64_t n_rows,
-            int64_t n_cols,
-            TrigDistName tn = TrigDistName::Hadamard
-        ) : n_rows(n_rows), n_cols(n_cols), family(tn) {
-            dim_short = MIN(n_rows, n_cols);
-            dim_long = MAX(n_rows, n_cols);
-}
-};
-
-    template<typename T, typename RNG = r123::Philox4x32, SignedInteger sint_t = int64_t>
-    struct TrigSkOp {
-        using generator = RNG;
-        using state_type = RNGState<RNG>;
-        using buffer_type = T;
-
-        //TODO: Where should the logic for deciding the size of `H` to use go?
-        // Since that will be accompanied by padding of (DA) maybe it should
-        // go inside of `lskget`?
-        const int64_t n_rows;
-        const int64_t n_cols;
-        int64_t dim_short;
-        int64_t dim_long;
-        // int64_t n_sampled;
-
-        const TrigDist dist;
-
-
-        const RNGState<RNG> seed_state;
-        RNGState<RNG> next_state;
-
-        const blas::Layout layout;
-        const bool sketchFromLeft = true;
-        bool known_filled = false;
-
-        sint_t* DiagonalRademacher = nullptr;
-        sint_t* SampledRows = nullptr;
-
-        TrigSkOp(
-            TrigDist dist,
-            RNGState<RNG> const &state,
-            blas::Layout layout,
-            bool known_filled = false
-        ) : n_rows(dist.n_rows), n_cols(dist.n_cols), dist(dist), seed_state(state), known_filled(known_filled), dim_short(dist.dim_short), dim_long(dist.dim_long), layout(layout){
-            // Memory for Rademacher diagonal gets allocated here
-            // Number of rows/cols to be sampled gets decided here
-            // i.e. `n_sampled` gets set
-            if(sketchFromLeft)
-                DiagonalRademacher = new sint_t[n_rows];
-            else
-                DiagonalRademacher = new sint_t[n_cols];
-
-            SampledRows = new sint_t[dim_short];
-
-            //TODO: Logic to compute the number of samples that we require `r`
-            // this can be shown to depend on the maximal row norm of the data matrix
-            //NOTE: Do not have access to this in the data-oblivious regime --- how
-            // do we get access?
-};
-
-        TrigSkOp(
-            TrigDistName family,
-            int64_t n_rows,
-            int64_t n_cols,
-            uint32_t key
-        ) : n_rows(n_rows), n_cols(n_cols), dist(TrigDist{n_rows, n_cols, family}), seed_state() {};
-
-        //TODO: Write a proper deconstructor
-        //Free up DiagonalRademacher && SampledRows
-        ~TrigSkOp();
-};
-
-template <typename T, typename RNG, SignedInteger sint_t>
-TrigSkOp<T, RNG, sint_t>::~TrigSkOp() {
-    delete [] this->DiagonalRademacher;
-    delete [] this->SampledRows;
-};
-
-template<typename T, typename RNG, SignedInteger sint_t>
-RandBLAS::RNGState<RNG> fill_trig(
-    TrigSkOp<T, RNG, sint_t> &Tr
-) {
-    /**
-     * Will do the work of filling in the diagonal Rademacher entries
-     * and selecting the rows/cols to be sampled
-     */
-    auto [ctr, key] = Tr.seed_state;
-
-    // Fill in the Rademacher diagonal
-    if(Tr.sketchFromLeft)
-        generateRademacherVector_r123(Tr.DiagonalRademacher, key[0], ctr[0], Tr.n_rows);
-    else
-        generateRademacherVector_r123(Tr.DiagonalRademacher, key[0], ctr[0], Tr.n_cols);
-
-    //NOTE: Select the rows/cols to be sampled --- use the `repeated_fisher_yates` function
-    int64_t r = Tr.dim_short;
-    int64_t d = Tr.dim_long;
-
-    std::vector<sint_t> idxs_minor(r); // Placeholder
-    std::vector<T> vals(r); // Placeholder
-
-    Tr.next_state = RandBLAS::repeated_fisher_yates<T, RNG, sint_t>(
-        Tr.seed_state,
-        r,         // Number of samples (vec_nnz)
-        d,         // Total number of elements (dim_major)
-        1,         // Single sample round (dim_minor)
-        Tr.SampledRows,  // Holds the required output
-        idxs_minor.data(),  // Placeholder
-        vals.data()         // Placeholder
-    );
-    Tr.known_filled = true;
-    return Tr.next_state;
-}
 }
 
 
 namespace RandBLAS::trig {
-
-// Performs the actual application of the fast trigonometric transform
-// Only called after calling `fill_trig`
-template <typename T, typename RNG>
-inline void lskget(
-    blas::Layout layout,
-    blas::Op opS,
-    blas::Op opA,
-    int64_t d, // B is d-by-n
-    int64_t n, // op(A) is m-by-n
-    int64_t m, // op(S) is d-by-m
-    T alpha,
-    TrigSkOp<T, RNG> &Tr,
-    int64_t ro_s,
-    int64_t co_s,
-    const T* A, // data-matrix
-    int64_t lda,
-    T beta,
-    T* B, // output matrix
-    int64_t ldb
-) {
-    if (!Tr.known_filled)
-        fill_trig(Tr);
-
-    // Applying the diagonal transformation
-    applyDiagonalRademacher(layout, opA, n, m, Tr.DiagonalRademacher, A, B);
-    // applyDiagonalRademacher(m, n, A, Tr.DiagonalRademacher);
-
-    //TODO: Apply the Hadamard transform
-
-    //... and finally permute the rows
-    // permuteRowsToTop(m, n, Tr.SampledRows, B, ldb);
-}
-
-
 /*
  * These functions apply an in-place, SRHT-like transform to the input matrix
  * i.e. A <- (\Pi H D)A OR A <- A(D H \Pi) (which is equivalent to A <- A(\Pi H D)^{-1})
@@ -593,7 +425,7 @@ inline void lmiget(
     std::vector<sint_t> idxs_minor(d); // Placeholder
     std::vector<T> vals(d); // Placeholder
 
-        // Populating `selected_rows`
+    // Populating `selected_rows`
         //TODO: Do I return this at some point?
     RandBLAS::RNGState<RNG> next_state = RandBLAS::repeated_fisher_yates<T, RNG, sint_t>(
         random_state,
@@ -641,7 +473,7 @@ inline void rmiget(
     std::vector<sint_t> idxs_minor(d); // Placeholder
     std::vector<T> vals(d); // Placeholder
 
-        // Populating `selected_rows`
+    // Populating `selected_rows`
         //TODO: Do I return this at some point?
     RandBLAS::RNGState<RNG> next_state = RandBLAS::repeated_fisher_yates<T, RNG, sint_t>(
         random_state,
