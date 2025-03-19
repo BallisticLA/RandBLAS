f5804c8af53b494dbe0b169ba87bfde6d2de1d62
Riley Murray <rjmurr@sandia.gov>
2025-03-19 10:52:40 -0700

    tests for column-major data

--- test/test_sparse_trsm/test_sparse_trsm.cc
+++ test/test_sparse_trsm/test_sparse_trsm.cc
@@ -109,48 +109,54 @@ class TestSptrsm : public ::testing::Test
     }
 
     template<typename T>
-    static void test_csc_solve_matrix(int64_t n, T p, Op op,  Uplo uplo, int64_t k, uint32_t key) {
+    static void test_csc_solve_matrix(Layout layout, int64_t n, T p, Op op,  Uplo uplo, int64_t k, uint32_t key) {
         auto A_coo = make_test_matrix(n, p, uplo == Uplo::Upper, key);
         CSCMatrix<T> A(n, n);
         RandBLAS::sparse_data::conversions::coo_to_csc(A_coo, A);
         std::vector<T> rhs(k * n);
         T* rhs_ptr = rhs.data();
-        for (int64_t i=0; i < k * n; i++) {
+        for (int64_t i = 0; i < k * n; i++) {
             rhs_ptr[i] = i;
         }
-        RandBLAS::sparse_data::trsm(Layout::RowMajor, op, (T)1.0, A, uplo, Diag::NonUnit, k, rhs_ptr, k);
+        std::vector<T> rhs_copy(rhs);
+        int64_t ldb = (layout == Layout::RowMajor) ? k : n;
+        RandBLAS::sparse_data::trsm(layout, op, (T)1.0, A, uplo, Diag::NonUnit, k, rhs_ptr, ldb);
 
         std::vector<T> reference(k * n);
         T* ref_ptr = reference.data();
-        RandBLAS::spmm(Layout::RowMajor, op, Op::NoTrans, n, k, n, 1.0, A, rhs_ptr, k, 0.0, ref_ptr, k);
+        RandBLAS::spmm(layout, op, Op::NoTrans, n, k, n, 1.0, A, rhs_ptr, ldb, 0.0, ref_ptr, ldb);
         
-        for (int64_t i = 0; i < k * n; ++i) {
-	    std::cout << ref_ptr[i] << "\t" << i << std::endl;
-            randblas_require(std::abs(ref_ptr[i] - i) <= RandBLAS::sqrt_epsilon<T>());
-        }
+        T atol = RandBLAS::sqrt_epsilon<T>();
+        T rtol = atol;
+        test::comparison::buffs_approx_equal(k*n, rhs_copy.data(), 1, ref_ptr, 1, __PRETTY_FUNCTION__, __FILE__, __LINE__, atol, rtol);
         return;
     }
 
     template<typename T>
-    static void test_csr_solve_matrix(int64_t n, T p, Op op, Uplo uplo, int64_t k, uint32_t key) {
+    static void test_csr_solve_matrix(Layout layout, int64_t n, T p, Op op, Uplo uplo, int64_t k, uint32_t key) {
         auto A_coo = make_test_matrix(n, p, uplo == Uplo::Upper, key);
         CSRMatrix<T> A(n, n);
         RandBLAS::sparse_data::conversions::coo_to_csr(A_coo, A);
         std::vector<T> rhs(k * n);
         T* rhs_ptr = rhs.data();
-        for (int64_t i=0; i < k * n; i++) {
+        for (int64_t i= 0; i < k * n; i++) {
             rhs_ptr[i] = i;
         }
-        RandBLAS::sparse_data::trsm(Layout::RowMajor, op, (T)1.0, A, uplo, Diag::NonUnit, k, rhs_ptr, k);
+        std::vector<T> rhs_copy(rhs);
+        int64_t ldb = (layout == Layout::RowMajor) ? k : n;
+        RandBLAS::sparse_data::trsm(layout, op, (T)1.0, A, uplo, Diag::NonUnit, k, rhs_ptr, ldb);
 
         std::vector<T> reference(k * n);
         T* ref_ptr = reference.data();
-        RandBLAS::spmm(Layout::RowMajor, op, Op::NoTrans, n, k, n, 1.0, A, rhs_ptr, k, 0.0, ref_ptr, k);
+        RandBLAS::spmm(layout, op, Op::NoTrans, n, k, n, 1.0, A, rhs_ptr, ldb, 0.0, ref_ptr, ldb);
         
-        for (int64_t i = 0; i < k * n; ++i) {
-	        std::cout << ref_ptr[i] << "\t" << i << std::endl;
-            randblas_require(std::abs(ref_ptr[i] - i) <= RandBLAS::sqrt_epsilon<T>());
-        }
+        // for (int64_t i = 0; i < k * n; ++i) {
+	    //     std::cout << ref_ptr[i] << "\t" << i << std::endl;
+        //     randblas_require(std::abs(ref_ptr[i] - i) <= RandBLAS::sqrt_epsilon<T>());
+        // }
+        T atol = RandBLAS::sqrt_epsilon<T>();
+        T rtol = atol;
+        test::comparison::buffs_approx_equal(k*n, rhs_copy.data(), 1, ref_ptr, 1, __PRETTY_FUNCTION__, __FILE__, __LINE__, atol, rtol);
         return;
     }
 
@@ -158,6 +164,7 @@ class TestSptrsm : public ::testing::Test
 };
 
 
+// MARK: TRSV
 
 TEST_F(TestSptrsm, upper_csr_solve) {
     test_csr_solve(1, 1.0, true, 3, 0x364A);
@@ -200,82 +207,167 @@ TEST_F(TestSptrsm, lower_csc_solve) {
 }
 
 
-TEST_F(TestSptrsm, lower_csc_solve_matrix) {
-    test_csc_solve_matrix(1, 1.0,    Op::NoTrans, Uplo::Lower, 1, 0x364A);
-    test_csc_solve_matrix(2, 0.5,    Op::NoTrans, Uplo::Lower, 1, 0x3643);
-    test_csc_solve_matrix(5, 0.9999, Op::NoTrans, Uplo::Lower, 1, 0x219B);
+// MARK: TRSM, row major
 
-    test_csc_solve_matrix(1, 1.0,    Op::NoTrans, Uplo::Lower, 3, 0x364A);
-    test_csc_solve_matrix(2, 0.5,    Op::NoTrans, Uplo::Lower, 3, 0x3643);
-    test_csc_solve_matrix(5, 0.9999, Op::NoTrans, Uplo::Lower, 3, 0x219B);
+TEST_F(TestSptrsm, lower_csc_solve_matrix_rowmajor) {
+    test_csc_solve_matrix(Layout::RowMajor, 1, 1.0,    Op::NoTrans, Uplo::Lower, 1, 0x364A);
+    test_csc_solve_matrix(Layout::RowMajor, 2, 0.5,    Op::NoTrans, Uplo::Lower, 1, 0x3643);
+    test_csc_solve_matrix(Layout::RowMajor, 5, 0.9999, Op::NoTrans, Uplo::Lower, 1, 0x219B);
+
+    test_csc_solve_matrix(Layout::RowMajor, 1, 1.0,    Op::NoTrans, Uplo::Lower, 3, 0x364A);
+    test_csc_solve_matrix(Layout::RowMajor, 2, 0.5,    Op::NoTrans, Uplo::Lower, 3, 0x3643);
+    test_csc_solve_matrix(Layout::RowMajor, 5, 0.9999, Op::NoTrans, Uplo::Lower, 3, 0x219B);
 }
 
-TEST_F(TestSptrsm, upper_csc_solve_matrix) {
-    test_csc_solve_matrix(1, 1.0,    Op::NoTrans, Uplo::Upper, 1, 0x364A);
-    test_csc_solve_matrix(2, 0.5,    Op::NoTrans, Uplo::Upper, 1, 0x3643);
-    test_csc_solve_matrix(5, 0.9999, Op::NoTrans, Uplo::Upper, 1, 0x219B);
+TEST_F(TestSptrsm, upper_csc_solve_matrix_rowmajor) {
+    test_csc_solve_matrix(Layout::RowMajor, 1, 1.0,    Op::NoTrans, Uplo::Upper, 1, 0x364A);
+    test_csc_solve_matrix(Layout::RowMajor, 2, 0.5,    Op::NoTrans, Uplo::Upper, 1, 0x3643);
+    test_csc_solve_matrix(Layout::RowMajor, 5, 0.9999, Op::NoTrans, Uplo::Upper, 1, 0x219B);
 
-    test_csc_solve_matrix(1, 1.0,    Op::NoTrans, Uplo::Upper, 3, 0x364A);
-    test_csc_solve_matrix(2, 0.5,    Op::NoTrans, Uplo::Upper, 3, 0x3643);
-    test_csc_solve_matrix(5, 0.9999, Op::NoTrans, Uplo::Upper, 3, 0x219B);
+    test_csc_solve_matrix(Layout::RowMajor, 1, 1.0,    Op::NoTrans, Uplo::Upper, 3, 0x364A);
+    test_csc_solve_matrix(Layout::RowMajor, 2, 0.5,    Op::NoTrans, Uplo::Upper, 3, 0x3643);
+    test_csc_solve_matrix(Layout::RowMajor, 5, 0.9999, Op::NoTrans, Uplo::Upper, 3, 0x219B);
 }
 
-TEST_F(TestSptrsm, lower_csr_solve_matrix) {
-    test_csr_solve_matrix(1, 1.0,    Op::NoTrans, Uplo::Lower, 1, 0x364A);
-    test_csr_solve_matrix(2, 0.5,    Op::NoTrans, Uplo::Lower, 1, 0x3643);
-    test_csr_solve_matrix(5, 0.9999, Op::NoTrans, Uplo::Lower, 1, 0x219B);
+TEST_F(TestSptrsm, lower_csr_solve_matrix_rowmajor) {
+    test_csr_solve_matrix(Layout::RowMajor, 1, 1.0,    Op::NoTrans, Uplo::Lower, 1, 0x364A);
+    test_csr_solve_matrix(Layout::RowMajor, 2, 0.5,    Op::NoTrans, Uplo::Lower, 1, 0x3643);
+    test_csr_solve_matrix(Layout::RowMajor, 5, 0.9999, Op::NoTrans, Uplo::Lower, 1, 0x219B);
 
-    test_csr_solve_matrix(1, 1.0,    Op::NoTrans, Uplo::Lower, 3, 0x364A);
-    test_csr_solve_matrix(2, 0.5,    Op::NoTrans, Uplo::Lower, 3, 0x3643);
-    test_csr_solve_matrix(5, 0.9999, Op::NoTrans, Uplo::Lower, 3, 0x219B);
+    test_csr_solve_matrix(Layout::RowMajor, 1, 1.0,    Op::NoTrans, Uplo::Lower, 3, 0x364A);
+    test_csr_solve_matrix(Layout::RowMajor, 2, 0.5,    Op::NoTrans, Uplo::Lower, 3, 0x3643);
+    test_csr_solve_matrix(Layout::RowMajor, 5, 0.9999, Op::NoTrans, Uplo::Lower, 3, 0x219B);
 }
 
-TEST_F(TestSptrsm, upper_csr_solve_matrix) {
-    test_csr_solve_matrix(1, 1.0,    Op::NoTrans, Uplo::Upper, 1, 0x364A);
-    test_csr_solve_matrix(2, 0.5,    Op::NoTrans, Uplo::Upper, 1, 0x3643);
-    test_csr_solve_matrix(5, 0.9999, Op::NoTrans, Uplo::Upper, 1, 0x219B);
+TEST_F(TestSptrsm, upper_csr_solve_matrix_rowmajor) {
+    test_csr_solve_matrix(Layout::RowMajor, 1, 1.0,    Op::NoTrans, Uplo::Upper, 1, 0x364A);
+    test_csr_solve_matrix(Layout::RowMajor, 2, 0.5,    Op::NoTrans, Uplo::Upper, 1, 0x3643);
+    test_csr_solve_matrix(Layout::RowMajor, 5, 0.9999, Op::NoTrans, Uplo::Upper, 1, 0x219B);
 
-    test_csr_solve_matrix(1, 1.0,    Op::NoTrans, Uplo::Upper, 3, 0x364A);
-    test_csr_solve_matrix(2, 0.5,    Op::NoTrans, Uplo::Upper, 3, 0x3643);
-    test_csr_solve_matrix(5, 0.9999, Op::NoTrans, Uplo::Upper, 3, 0x219B);
+    test_csr_solve_matrix(Layout::RowMajor, 1, 1.0,    Op::NoTrans, Uplo::Upper, 3, 0x364A);
+    test_csr_solve_matrix(Layout::RowMajor, 2, 0.5,    Op::NoTrans, Uplo::Upper, 3, 0x3643);
+    test_csr_solve_matrix(Layout::RowMajor, 5, 0.9999, Op::NoTrans, Uplo::Upper, 3, 0x219B);
 }
 
-TEST_F(TestSptrsm, lower_csc_trans_solve_matrix) {
-    test_csc_solve_matrix(1, 1.0,    Op::Trans, Uplo::Lower, 1, 0x364A);
-    test_csc_solve_matrix(2, 0.5,    Op::Trans, Uplo::Lower, 1, 0x3643);
-    test_csc_solve_matrix(5, 0.9999, Op::Trans, Uplo::Lower, 1, 0x219B);
+TEST_F(TestSptrsm, lower_csc_trans_solve_matrix_rowmajor) {
+    test_csc_solve_matrix(Layout::RowMajor, 1, 1.0,    Op::Trans, Uplo::Lower, 1, 0x364A);
+    test_csc_solve_matrix(Layout::RowMajor, 2, 0.5,    Op::Trans, Uplo::Lower, 1, 0x3643);
+    test_csc_solve_matrix(Layout::RowMajor, 5, 0.9999, Op::Trans, Uplo::Lower, 1, 0x219B);
 
-    test_csc_solve_matrix(1, 1.0,    Op::Trans, Uplo::Lower, 3, 0x364A);
-    test_csc_solve_matrix(2, 0.5,    Op::Trans, Uplo::Lower, 3, 0x3643);
-    test_csc_solve_matrix(5, 0.9999, Op::Trans, Uplo::Lower, 3, 0x219B);
+    test_csc_solve_matrix(Layout::RowMajor, 1, 1.0,    Op::Trans, Uplo::Lower, 3, 0x364A);
+    test_csc_solve_matrix(Layout::RowMajor, 2, 0.5,    Op::Trans, Uplo::Lower, 3, 0x3643);
+    test_csc_solve_matrix(Layout::RowMajor, 5, 0.9999, Op::Trans, Uplo::Lower, 3, 0x219B);
 }
 
-TEST_F(TestSptrsm, upper_csc_trans_solve_matrix) {
-    test_csc_solve_matrix(1, 1.0,    Op::Trans, Uplo::Upper, 1, 0x364A);
-    test_csc_solve_matrix(2, 0.5,    Op::Trans, Uplo::Upper, 1, 0x3643);
-    test_csc_solve_matrix(5, 0.9999, Op::Trans, Uplo::Upper, 1, 0x219B);
+TEST_F(TestSptrsm, upper_csc_trans_solve_matrix_rowmajor) {
+    test_csc_solve_matrix(Layout::RowMajor, 1, 1.0,    Op::Trans, Uplo::Upper, 1, 0x364A);
+    test_csc_solve_matrix(Layout::RowMajor, 2, 0.5,    Op::Trans, Uplo::Upper, 1, 0x3643);
+    test_csc_solve_matrix(Layout::RowMajor, 5, 0.9999, Op::Trans, Uplo::Upper, 1, 0x219B);
 
-    test_csc_solve_matrix(1, 1.0,    Op::Trans, Uplo::Upper, 3, 0x364A);
-    test_csc_solve_matrix(2, 0.5,    Op::Trans, Uplo::Upper, 3, 0x3643);
-    test_csc_solve_matrix(5, 0.9999, Op::Trans, Uplo::Upper, 3, 0x219B);
+    test_csc_solve_matrix(Layout::RowMajor, 1, 1.0,    Op::Trans, Uplo::Upper, 3, 0x364A);
+    test_csc_solve_matrix(Layout::RowMajor, 2, 0.5,    Op::Trans, Uplo::Upper, 3, 0x3643);
+    test_csc_solve_matrix(Layout::RowMajor, 5, 0.9999, Op::Trans, Uplo::Upper, 3, 0x219B);
 }
 
-TEST_F(TestSptrsm, lower_csr_trans_solve_matrix) {
-    test_csr_solve_matrix(1, 1.0,    Op::Trans, Uplo::Lower, 1, 0x364A);
-    test_csr_solve_matrix(2, 0.5,    Op::Trans, Uplo::Lower, 1, 0x3643);
-    test_csr_solve_matrix(5, 0.9999, Op::Trans, Uplo::Lower, 1, 0x219B);
+TEST_F(TestSptrsm, lower_csr_trans_solve_matrix_rowmajor) {
+    test_csr_solve_matrix(Layout::RowMajor, 1, 1.0,    Op::Trans, Uplo::Lower, 1, 0x364A);
+    test_csr_solve_matrix(Layout::RowMajor, 2, 0.5,    Op::Trans, Uplo::Lower, 1, 0x3643);
+    test_csr_solve_matrix(Layout::RowMajor, 5, 0.9999, Op::Trans, Uplo::Lower, 1, 0x219B);
 
-    test_csr_solve_matrix(1, 1.0,    Op::Trans, Uplo::Lower, 3, 0x364A);
-    test_csr_solve_matrix(2, 0.5,    Op::Trans, Uplo::Lower, 3, 0x3643);
-    test_csr_solve_matrix(5, 0.9999, Op::Trans, Uplo::Lower, 3, 0x219B);
+    test_csr_solve_matrix(Layout::RowMajor, 1, 1.0,    Op::Trans, Uplo::Lower, 3, 0x364A);
+    test_csr_solve_matrix(Layout::RowMajor, 2, 0.5,    Op::Trans, Uplo::Lower, 3, 0x3643);
+    test_csr_solve_matrix(Layout::RowMajor, 5, 0.9999, Op::Trans, Uplo::Lower, 3, 0x219B);
 }
 
-TEST_F(TestSptrsm, upper_csr_trans_solve_matrix) {
-    test_csr_solve_matrix(1, 1.0,    Op::Trans, Uplo::Upper, 1, 0x364A);
-    test_csr_solve_matrix(2, 0.5,    Op::Trans, Uplo::Upper, 1, 0x3643);
-    test_csr_solve_matrix(5, 0.9999, Op::Trans, Uplo::Upper, 1, 0x219B);
+TEST_F(TestSptrsm, upper_csr_trans_solve_matrix_rowmajor) {
+    test_csr_solve_matrix(Layout::RowMajor, 1, 1.0,    Op::Trans, Uplo::Upper, 1, 0x364A);
+    test_csr_solve_matrix(Layout::RowMajor, 2, 0.5,    Op::Trans, Uplo::Upper, 1, 0x3643);
+    test_csr_solve_matrix(Layout::RowMajor, 5, 0.9999, Op::Trans, Uplo::Upper, 1, 0x219B);
 
-    test_csr_solve_matrix(1, 1.0,    Op::Trans, Uplo::Upper, 3, 0x364A);
-    test_csr_solve_matrix(2, 0.5,    Op::Trans, Uplo::Upper, 3, 0x3643);
-    test_csr_solve_matrix(5, 0.9999, Op::Trans, Uplo::Upper, 3, 0x219B);
+    test_csr_solve_matrix(Layout::RowMajor, 1, 1.0,    Op::Trans, Uplo::Upper, 3, 0x364A);
+    test_csr_solve_matrix(Layout::RowMajor, 2, 0.5,    Op::Trans, Uplo::Upper, 3, 0x3643);
+    test_csr_solve_matrix(Layout::RowMajor, 5, 0.9999, Op::Trans, Uplo::Upper, 3, 0x219B);
+}
+
+// MARK: TRSM, column major
+
+
+TEST_F(TestSptrsm, lower_csc_solve_matrix_comajor) {
+    test_csc_solve_matrix(Layout::ColMajor, 1, 1.0,    Op::NoTrans, Uplo::Lower, 1, 0x364A);
+    test_csc_solve_matrix(Layout::ColMajor, 2, 0.5,    Op::NoTrans, Uplo::Lower, 1, 0x3643);
+    test_csc_solve_matrix(Layout::ColMajor, 5, 0.9999, Op::NoTrans, Uplo::Lower, 1, 0x219B);
+
+    test_csc_solve_matrix(Layout::ColMajor, 1, 1.0,    Op::NoTrans, Uplo::Lower, 3, 0x364A);
+    test_csc_solve_matrix(Layout::ColMajor, 2, 0.5,    Op::NoTrans, Uplo::Lower, 3, 0x3643);
+    test_csc_solve_matrix(Layout::ColMajor, 5, 0.9999, Op::NoTrans, Uplo::Lower, 3, 0x219B);
+}
+
+TEST_F(TestSptrsm, upper_csc_solve_matrix_comajor) {
+    test_csc_solve_matrix(Layout::ColMajor, 1, 1.0,    Op::NoTrans, Uplo::Upper, 1, 0x364A);
+    test_csc_solve_matrix(Layout::ColMajor, 2, 0.5,    Op::NoTrans, Uplo::Upper, 1, 0x3643);
+    test_csc_solve_matrix(Layout::ColMajor, 5, 0.9999, Op::NoTrans, Uplo::Upper, 1, 0x219B);
+
+    test_csc_solve_matrix(Layout::ColMajor, 1, 1.0,    Op::NoTrans, Uplo::Upper, 3, 0x364A);
+    test_csc_solve_matrix(Layout::ColMajor, 2, 0.5,    Op::NoTrans, Uplo::Upper, 3, 0x3643);
+    test_csc_solve_matrix(Layout::ColMajor, 5, 0.9999, Op::NoTrans, Uplo::Upper, 3, 0x219B);
+}
+
+TEST_F(TestSptrsm, lower_csr_solve_matrix_comajor) {
+    test_csr_solve_matrix(Layout::ColMajor, 1, 1.0,    Op::NoTrans, Uplo::Lower, 1, 0x364A);
+    test_csr_solve_matrix(Layout::ColMajor, 2, 0.5,    Op::NoTrans, Uplo::Lower, 1, 0x3643);
+    test_csr_solve_matrix(Layout::ColMajor, 5, 0.9999, Op::NoTrans, Uplo::Lower, 1, 0x219B);
+
+    test_csr_solve_matrix(Layout::ColMajor, 1, 1.0,    Op::NoTrans, Uplo::Lower, 3, 0x364A);
+    test_csr_solve_matrix(Layout::ColMajor, 2, 0.5,    Op::NoTrans, Uplo::Lower, 3, 0x3643);
+    test_csr_solve_matrix(Layout::ColMajor, 5, 0.9999, Op::NoTrans, Uplo::Lower, 3, 0x219B);
+}
+
+TEST_F(TestSptrsm, upper_csr_solve_matrix_comajor) {
+    test_csr_solve_matrix(Layout::ColMajor, 1, 1.0,    Op::NoTrans, Uplo::Upper, 1, 0x364A);
+    test_csr_solve_matrix(Layout::ColMajor, 2, 0.5,    Op::NoTrans, Uplo::Upper, 1, 0x3643);
+    test_csr_solve_matrix(Layout::ColMajor, 5, 0.9999, Op::NoTrans, Uplo::Upper, 1, 0x219B);
+
+    test_csr_solve_matrix(Layout::ColMajor, 1, 1.0,    Op::NoTrans, Uplo::Upper, 3, 0x364A);
+    test_csr_solve_matrix(Layout::ColMajor, 2, 0.5,    Op::NoTrans, Uplo::Upper, 3, 0x3643);
+    test_csr_solve_matrix(Layout::ColMajor, 5, 0.9999, Op::NoTrans, Uplo::Upper, 3, 0x219B);
+}
+
+TEST_F(TestSptrsm, lower_csc_trans_solve_matrix_comajor) {
+    test_csc_solve_matrix(Layout::ColMajor, 1, 1.0,    Op::Trans, Uplo::Lower, 1, 0x364A);
+    test_csc_solve_matrix(Layout::ColMajor, 2, 0.5,    Op::Trans, Uplo::Lower, 1, 0x3643);
+    test_csc_solve_matrix(Layout::ColMajor, 5, 0.9999, Op::Trans, Uplo::Lower, 1, 0x219B);
+
+    test_csc_solve_matrix(Layout::ColMajor, 1, 1.0,    Op::Trans, Uplo::Lower, 3, 0x364A);
+    test_csc_solve_matrix(Layout::ColMajor, 2, 0.5,    Op::Trans, Uplo::Lower, 3, 0x3643);
+    test_csc_solve_matrix(Layout::ColMajor, 5, 0.9999, Op::Trans, Uplo::Lower, 3, 0x219B);
+}
+
+TEST_F(TestSptrsm, upper_csc_trans_solve_matrix_comajor) {
+    test_csc_solve_matrix(Layout::ColMajor, 1, 1.0,    Op::Trans, Uplo::Upper, 1, 0x364A);
+    test_csc_solve_matrix(Layout::ColMajor, 2, 0.5,    Op::Trans, Uplo::Upper, 1, 0x3643);
+    test_csc_solve_matrix(Layout::ColMajor, 5, 0.9999, Op::Trans, Uplo::Upper, 1, 0x219B);
+
+    test_csc_solve_matrix(Layout::ColMajor, 1, 1.0,    Op::Trans, Uplo::Upper, 3, 0x364A);
+    test_csc_solve_matrix(Layout::ColMajor, 2, 0.5,    Op::Trans, Uplo::Upper, 3, 0x3643);
+    test_csc_solve_matrix(Layout::ColMajor, 5, 0.9999, Op::Trans, Uplo::Upper, 3, 0x219B);
+}
+
+TEST_F(TestSptrsm, lower_csr_trans_solve_matrix_comajor) {
+    test_csr_solve_matrix(Layout::ColMajor, 1, 1.0,    Op::Trans, Uplo::Lower, 1, 0x364A);
+    test_csr_solve_matrix(Layout::ColMajor, 2, 0.5,    Op::Trans, Uplo::Lower, 1, 0x3643);
+    test_csr_solve_matrix(Layout::ColMajor, 5, 0.9999, Op::Trans, Uplo::Lower, 1, 0x219B);
+
+    test_csr_solve_matrix(Layout::ColMajor, 1, 1.0,    Op::Trans, Uplo::Lower, 3, 0x364A);
+    test_csr_solve_matrix(Layout::ColMajor, 2, 0.5,    Op::Trans, Uplo::Lower, 3, 0x3643);
+    test_csr_solve_matrix(Layout::ColMajor, 5, 0.9999, Op::Trans, Uplo::Lower, 3, 0x219B);
+}
+
+TEST_F(TestSptrsm, upper_csr_trans_solve_matrix_comajor) {
+    test_csr_solve_matrix(Layout::ColMajor, 1, 1.0,    Op::Trans, Uplo::Upper, 1, 0x364A);
+    test_csr_solve_matrix(Layout::ColMajor, 2, 0.5,    Op::Trans, Uplo::Upper, 1, 0x3643);
+    test_csr_solve_matrix(Layout::ColMajor, 5, 0.9999, Op::Trans, Uplo::Upper, 1, 0x219B);
+
+    test_csr_solve_matrix(Layout::ColMajor, 1, 1.0,    Op::Trans, Uplo::Upper, 3, 0x364A);
+    test_csr_solve_matrix(Layout::ColMajor, 2, 0.5,    Op::Trans, Uplo::Upper, 3, 0x3643);
+    test_csr_solve_matrix(Layout::ColMajor, 5, 0.9999, Op::Trans, Uplo::Upper, 3, 0x219B);
 }
